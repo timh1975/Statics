460325f1a4067e8a371e250e00a7c2d7c8caf033
Tim Honisett <tim.honisett@hotmail.co.uk>
2025-02-26 14:10:48 +0000

    add disable default queries

--- src/app.py
+++ src/app.py
@@ -1,24 +1,10 @@
+import random
+
 def helloWorld():
     return "Hello, World!"
 
 def goodbye():
     return "Goodbye, World!"
 
-def greet(name):
-    return f"Hello, {name}!"
 
-def farewell(name):
-    return f"Goodbye, {name}!"
-
-def goodmorning():
-    return "Good morning!"
-
-def goodevening():
-    return "Good evening!"
-
-def goodnight():
-    return "Good night!"
-
-def goodafternoon():
-    return "Good afternoon!"
 
