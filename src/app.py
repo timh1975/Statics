5937eda020a5b68060aa11249cfa2acea6798d9d
Tim Honisett <tim.honisett@hotmail.co.uk>
2025-04-06 11:43:48 +0100

    made refactoring amendments to list.append

--- src/app.py
+++ src/app.py
@@ -8,4 +8,7 @@ for x in range(1, 1000000):
 b = []
 for x in range(1, 1000000):
         random_number = random.randint(0,255)
-        a.append(x, random_number)
+        b.append((x, random_number))
+
+
+        
