a6e70efc7b0b8c64924972f072b45efd77ebe291
Tim Honisett <tim.honisett@hotmail.co.uk>
2025-04-06 23:56:29 +0100

    removed list append

--- src/app.py
+++ src/app.py
@@ -5,10 +5,5 @@ for x in range(1, 1000000):
         a.insert(x, random_number)
 
 
-b = []
-for x in range(1, 1000000):
-        random_number = random.randint(0,255)
-        b.append((x, random_number))
-
 
         
